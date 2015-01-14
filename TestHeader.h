03ce8ba9587ca71d1504584e1eb5d406345d09e1
longmatthew <longmatthew@live.com>
2015-01-13 20:35:20 -0800

    Initial commit of Programming Assignment 1 Problem 2

---
+++ TestHeader.h
@@ -0,0 +1,9 @@
+#ifndef TESTHEADER // Header Security 'Lock'.
+#define TESTHEADER
+
+#include "FileHeader.h" // Inclusion of main header file.
+
+// Test function that will determine if a few scripted sentences are a palindrome.
+void testPalindrome(void);
+
+#endif // End of 'Lock'
